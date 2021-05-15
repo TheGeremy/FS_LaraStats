a920e30bcc4cf536dcc79a7c6c2cecbe73f771b5
TheGeremy <the.geremy@gmail.com>
2021-05-15 09:27:41 +0200

    Create load_config_map_farmlands.php

---
+++ database/csv_load/load/load_config_map/load_config_map_farmlands.php
@@ -0,0 +1,31 @@
+<?php
+$xml_map_farmlands = load_xml_file($map_dir . "farmlands.xml");
+
+$price_per_ha = (integer)$xml_map_farmlands->{'farmlands'}->attributes()->{'pricePerHa'};
+
+// this only if xml field name differ from database field name
+$field_mapping = array(
+	"id" => "game_field_id",
+	"npcName" => "npc_name",
+	"priceScale" => "price_scale",
+	"sizeHa" => "ha",
+	"note" => "note",
+	"defaultFarmProperty" => "owning"
+);
+
+$data = array();
+$row = 1;
+foreach ($xml_map_farmlands->farmlands->farmland as $farmland) {
+	$fs_farmland_dim_data[$row]['map_id'] = $map_id;
+	foreach ($farmland->attributes() as $key => $value) {
+		if(array_key_exists($key, $field_mapping)) {
+			$fs_farmland_dim_data[$row][$field_mapping[$key]] = (string)$value;
+		}
+	}
+	++$row;
+}
+
+$query = prepare_query_ml('fs_map_farmland_dim', $fs_farmland_dim_data);
+execute_query($query);
+just_print("Data loaded to fs_map_farmland_dim (" . (string)($row - 1) . " rows).");
+?>
