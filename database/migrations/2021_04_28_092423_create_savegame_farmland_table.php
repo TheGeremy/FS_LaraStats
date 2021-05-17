db757d20fae0dc8c68ec0600c2ec07884b209982
TheGeremy <the.geremy@gmail.com>
2021-05-17 12:55:21 +0200

    migration
    migration for fs_savegame_farmland to store info from farmlands.xml, which farmland belongs to which farm

---
+++ database/migrations/2021_04_28_092423_create_savegame_farmland_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSavegameFarmlandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fs_savegame_farmland', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('save_id');              
+            $table->unsignedTinyInteger('farm_id');            
+            $table->unsignedTinyInteger('game_field_id');            
+            // end of indexes section
+            $table->string('land_name', 100)->nullable();
+            // system colulmns            
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            // define indexes
+            $table->index('save_id');
+            $table->index('farm_id');
+            $table->index('game_field_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fs_savegame_farmland');
+    }
+}
