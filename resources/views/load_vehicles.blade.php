8aadc8686584bba80e19276ee374bb1c7f4bf44b
TheGeremy <the.geremy@gmail.com>
2021-05-26 13:37:46 +0200

    temporary layout change

---
+++ resources/views/load_vehicles.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('title', 'FS19 - Webstats: Test')
+
+@section('header_text','Testing PHP')
+
+@section('content')
+    <div class="w3-container" style="font-size:12px;color:#E7D1B0;background-color:#191919;">
+    	<pre style="text-indent:0;">
+	      	@php
+	      		echo "\n";
+				// load
+				require base_path() . '/database/csv_load/load/load_savegame/start_load_vehicles.php';
+			@endphp
+		</pre>
+    </div>
+@endsection
